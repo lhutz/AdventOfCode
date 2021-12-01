4b357e3093cb731e32ab1e73302005f9584e8d2c
Lindy Hutz <lindy.hutz@teamrockstars.nl>
2021-12-01 23:39:42 +0100

    day1

---
+++ AdventOfCodeYear2021.Test/Day1Test.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCodeYear2021.Test
+{
+    [TestFixture]
+    public class Day1Test
+    {
+        [Test]
+        public void ExecutePart1_GivenExample_ShouldReturn7()
+        {
+            // Arrange
+            var givenExample = new [] { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
+            var expectedValue = 7;
+            var testInstance = new Day1 {Input = givenExample};
+
+            // Act
+            var receivedValue = testInstance.ExecutePart1();
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+        [Test]
+        public void ExecutePart2_GivenExample_ShouldReturn5()
+        {
+            // Arrange
+            var givenExample = new [] { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
+            var expectedValue = 5;
+            var testInstance = new Day1 {Input = givenExample};
+
+            // Act
+            var receivedValue = testInstance.ExecutePart2();
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+    }
+}
---
+++ AdventOfCodeYear2021/Day1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tidy.AdventOfCode;
+
+namespace AdventOfCodeYear2021
+{
+    [Day(2021, 1)]
+    public class Day1 : Day.NewLineSplitParsed<int>
+    {
+        public override object ExecutePart1()
+        {
+            return AmountWasHigher(Input);
+        }
+
+        public override object ExecutePart2()
+        {
+            var slidingWindows = GetSlidingWindows(Input);
+            return AmountWasHigher(slidingWindows);
+        }
+
+        private int AmountWasHigher(IEnumerable<int> measurements)
+        {
+            var wasHigher = 0;
+            int? previous = null;
+            foreach (var current in measurements)
+            {
+                if (previous < current) wasHigher++;
+                previous = current;
+            }
+
+            return wasHigher;
+        }
+
+        private IEnumerable<int> GetSlidingWindows(int[] measurements)
+        {
+            return measurements.Select((measurement, index) =>
+                    measurements.Length > index + 2
+                    ? measurement + measurements[index + 1] + measurements[index + 2]
+                    : -1
+                )
+                .Where(window => window > -1);
+        }
+    }
+}
