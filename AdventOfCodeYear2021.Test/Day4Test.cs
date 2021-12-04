0abd3b5f574c6fcb5c3505c3dd32de600eef1554
Lindy Hutz <lindy.hutz@teamrockstars.nl>
2021-12-04 22:44:53 +0100

    Day4

---
+++ AdventOfCodeYear2021.Test/Day4Test.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCodeYear2021.Test
+{
+    [TestFixture]
+    public class Day4Test
+    {
+        private string[] givenExample = {
+            "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1",
+            "",
+            "22 13 17 11  0",
+            "8  2 23  4 24",
+            "21  9 14 16  7",
+            "6 10  3 18  5",
+            "1 12 20 15 19",
+            "",
+            "3 15  0  2 22",
+            "9 18 13 17  5",
+            "19  8  7 25 23",
+            "20 11 10 24  4",
+            "14 21 16 12  6",
+            "",
+            "14 21 17 24  4",
+            "10 16 15  9 19",
+            "18  8 23 26 20",
+            "22 11 13  6  5",
+            "2  0 12  3  7",
+        };
+
+        [Test]
+        public void ExecutePart1_GivenExample_ShouldReturnExpected()
+        {
+            // Arrange
+            var expectedValue = 4512;
+            var testInstance = new Day4 {Input = givenExample};
+
+            // Act
+            var receivedValue = testInstance.ExecutePart1();
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+
+        [Test]
+        public void ExecutePart2_GivenExample_ShouldReturnExpectedValue()
+        {
+            // Arrange
+            var expectedValue = 1924;
+            var testInstance = new Day4 {Input = givenExample};
+
+            // Act
+            var receivedValue = testInstance.ExecutePart2();
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+    }
+}
---
+++ AdventOfCodeYear2021/Day4.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tidy.AdventOfCode;
+
+namespace AdventOfCodeYear2021
+{
+    [Day(2021, 4)]
+    public class Day4 : Day.NewLineSplitParsed<string>
+    {
+        public override object ExecutePart1()
+        {
+            var drawnNumbers = GetDrawnNumbers(Input);
+            var inputBingoCards = GetBingoCards(Input);
+            var winningBingoCard = GetWinningBingoCard(drawnNumbers, inputBingoCards);
+
+            return winningBingoCard.GetScore();
+        }
+
+        public override object ExecutePart2()
+        {
+            var drawnNumbers = GetDrawnNumbers(Input);
+            var inputBingoCards = GetBingoCards(Input);
+            BingoCard? winningBingoCard = null;
+            var amountOfBingoCards = inputBingoCards.Count;
+            for (var i = 0; i < amountOfBingoCards; i++)
+            {
+                winningBingoCard = GetWinningBingoCard(drawnNumbers, inputBingoCards);
+                inputBingoCards.Remove(winningBingoCard);
+            }
+
+            return winningBingoCard.GetScore();
+
+        }
+
+        private List<int> GetDrawnNumbers(string[] input)
+        {
+            return input[0].Split(",").Select(int.Parse).ToList();
+        }
+
+        private List<BingoCard> GetBingoCards(string[] input)
+        {
+            var bingoCards = new List<BingoCard>();
+
+            var bingoCardInputs = input.Skip(2).Take(input.Length - 2).ToArray();
+            for (var i = 0; i < bingoCardInputs.Length; i += 6)
+            {
+                var bingoCardInput = bingoCardInputs.Skip(i).Take(5).ToArray();
+                bingoCards.Add(new BingoCard(bingoCardInput));
+            }
+
+            return bingoCards;
+        }
+
+        private BingoCard GetWinningBingoCard(List<int>? drawnNumbers, List<BingoCard>? inputBingoCards)
+        {
+            BingoCard? winningBingoCard = null;
+            foreach (var drawnNumber in drawnNumbers)
+            {
+                winningBingoCard = inputBingoCards.FirstOrDefault(x => x.AddAndCheckForBingo(drawnNumber));
+                if (winningBingoCard != null)
+                {
+                    break;
+                }
+            }
+
+            return winningBingoCard;
+        }
+    }
+
+    public class BingoCard
+    {
+        private List<List<int>> rows;
+        private List<List<int>> collumns;
+        private List<int> drawnNumbers;
+
+        public BingoCard(string[] givenRows)
+        {
+            collumns = new List<List<int>>();
+            rows = new List<List<int>>();
+            this.drawnNumbers = new List<int>();
+
+            foreach (var givenRow in givenRows)
+            {
+                var seperateNumbers = givenRow.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                rows.Add(seperateNumbers.Select(int.Parse).ToList());
+                if (collumns.Count == 0)
+                {
+                    foreach (var seperateNumber in seperateNumbers)
+                    {
+                        collumns.Add(new List<int>{int.Parse(seperateNumber)});
+                    }
+                }
+                else
+                {
+                    for (var i = 0; i < seperateNumbers.Length; i++)
+                    {
+                        collumns[i].Add(int.Parse(seperateNumbers[i]));
+                    }
+                }
+            }
+        }
+
+        public bool AddAndCheckForBingo(int drawnNumber)
+        {
+            this.drawnNumbers.Add(drawnNumber);
+            return rows.Any(FullyDrawn) || collumns.Any(FullyDrawn);
+        }
+
+        public int GetScore()
+        {
+            var allNumbers = rows.SelectMany(x => x);
+            var unmarkedNumbers = allNumbers.Except(drawnNumbers);
+            return unmarkedNumbers.Aggregate(0, (acc, value) => acc += value ) * drawnNumbers.Last();
+        }
+
+        private bool FullyDrawn(List<int> numbers)
+        {
+            return numbers.All(drawnNumbers.Contains);
+        }
+    }
+}
