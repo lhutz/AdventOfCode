9224652eb878b3dc0c22d51ef6b2bdc22540e4f8
Lindy Hutz <lindy.hutz@teamrockstars.nl>
2021-12-05 14:10:30 +0100

    Day5

---
+++ AdventOfCodeYear2021.Test/Day5Test.cs
@@ -0,0 +1,250 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCodeYear2021.Test
+{
+    [TestFixture]
+    public class Day5Test
+    {
+        private string[] givenExample = {
+            "0,9 -> 5,9",
+            "8,0 -> 0,8",
+            "9,4 -> 3,4",
+            "2,2 -> 2,1",
+            "7,0 -> 7,4",
+            "6,4 -> 2,0",
+            "0,9 -> 2,9",
+            "3,4 -> 1,4",
+            "0,0 -> 8,8",
+            "5,5 -> 8,2",
+        };
+
+        [Test]
+        public void ExecutePart1_GivenExample_ShouldReturnExpected()
+        {
+            // Arrange
+            var expectedValue = 5;
+            var testInstance = new Day5 {Input = givenExample};
+
+            // Act
+            var receivedValue = testInstance.ExecutePart1();
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+
+        [Test]
+        public void ExecutePart2_GivenExample_ShouldReturnExpectedValue()
+        {
+            // Arrange
+            var expectedValue = 12;
+            var testInstance = new Day5 {Input = givenExample};
+
+            // Act
+            var receivedValue = testInstance.ExecutePart2();
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+
+        [Test]
+        public void Line_AddCoordinatesInBetween_GivenHorizontalStartAndEnd_AllExpectedCoordinates()
+        {
+            // Arrange
+            var start = "0,9";
+            var end = "5,9";
+            var expectedCoordinates = new List<Coordinate>
+            {
+                new(0, 9),
+                new(1, 9),
+                new(2, 9),
+                new(3, 9),
+                new(4, 9),
+                new(5, 9)
+            };
+
+            // Act
+            var line = new Line(start, end, true);
+
+            // Assert
+            line.Coordinates.Should().BeEquivalentTo(expectedCoordinates);
+        }
+
+        [Test]
+        public void Line_AddCoordinatesInBetween_GivenVerticalStartAndEnd_AllExpectedCoordinates()
+        {
+            // Arrange
+            var start = "7,0";
+            var end = "7,4";
+            var expectedCoordinates = new List<Coordinate>
+            {
+                new(7, 0),
+                new(7, 1),
+                new(7, 2),
+                new(7, 3),
+                new(7, 4)
+            };
+
+            // Act
+            var line = new Line(start, end, true);
+
+            // Assert
+            line.Coordinates.Should().BeEquivalentTo(expectedCoordinates);
+        }
+
+        [Test]
+        public void Line_AddCoordinatesInBetween_GivenHorizontalStartAndEndRightToLeft_AllExpectedCoordinates()
+        {
+            // Arrange
+            var start = "5,9";
+            var end = "0,9";
+
+            var expectedCoordinates = new List<Coordinate>
+            {
+                new(0, 9),
+                new(1, 9),
+                new(2, 9),
+                new(3, 9),
+                new(4, 9),
+                new(5, 9)
+            };
+
+            // Act
+            var line = new Line(start, end, true);
+
+            // Assert
+            line.Coordinates.Should().BeEquivalentTo(expectedCoordinates);
+        }
+
+        [Test]
+        public void Line_AddCoordinatesInBetween_GivenVerticalStartAndEndBottomUp_AllExpectedCoordinates()
+        {
+            // Arrange
+            var start = "7,4";
+            var end = "7,0";
+            var expectedCoordinates = new List<Coordinate>
+            {
+                new(7, 0),
+                new(7, 1),
+                new(7, 2),
+                new(7, 3),
+                new(7, 4)
+            };
+
+            // Act
+            var line = new Line(start, end, true);
+
+            // Assert
+            line.Coordinates.Should().BeEquivalentTo(expectedCoordinates);
+        }
+
+        [Test]
+        public void Line_AddCoordinatesInBetween_GivenLeftUpperToRightLower_AllExpectedCoordinates()
+        {
+            // Arrange
+            var start = "1,1";
+            var end = "6,6";
+            var expectedCoordinates = new List<Coordinate>
+            {
+                new(1,1),
+                new(2,2),
+                new(3,3),
+                new(4,4),
+                new(5,5),
+                new(6,6),
+            };
+
+            // Act
+            var line = new Line(start, end, false);
+
+            // Assert
+            line.Coordinates.Should().BeEquivalentTo(expectedCoordinates);
+        }
+
+        [Test]
+        public void Line_AddCoordinatesInBetween_GivenRighUpperToLeftLower_AllExpectedCoordinates()
+        {
+            // Arrange
+            var start = "6,0";
+            var end = "0,6";
+            var expectedCoordinates = new List<Coordinate>
+            {
+                new(6,0),
+                new(5,1),
+                new(4,2),
+                new(3,3),
+                new(2,4),
+                new(1,5),
+                new(0,6),
+            };
+
+            // Act
+            var line = new Line(start, end, false);
+
+            // Assert
+            line.Coordinates.Should().BeEquivalentTo(expectedCoordinates);
+        }
+
+        [Test]
+        public void Line_AddCoordinatesInBetween_GivenRightLowerToLeftUpper_AllExpectedCoordinates()
+        {
+            // Arrange
+            var start = "6,6";
+            var end = "1,1";
+            var expectedCoordinates = new List<Coordinate>
+            {
+                new(1,1),
+                new(2,2),
+                new(3,3),
+                new(4,4),
+                new(5,5),
+                new(6,6),
+            };
+
+            // Act
+            var line = new Line(start, end, false);
+
+            // Assert
+            line.Coordinates.Should().BeEquivalentTo(expectedCoordinates);
+        }
+
+        [Test]
+        public void Line_AddCoordinatesInBetween_GivenLeftLowerToRightUpper_AllExpectedCoordinates()
+        {
+            // Arrange
+            var start = "0,6";
+            var end = "6,0";
+            var expectedCoordinates = new List<Coordinate>
+            {
+                new(6,0),
+                new(5,1),
+                new(4,2),
+                new(3,3),
+                new(2,4),
+                new(1,5),
+                new(0,6),
+            };
+
+            // Act
+            var line = new Line(start, end, false);
+
+            // Assert
+            line.Coordinates.Should().BeEquivalentTo(expectedCoordinates);
+        }
+
+        [Test]
+        public void Line_AddCoordinatesInBetween_DiagonalLineButIgnoreDiagonal_AllExpectedCoordinates()
+        {
+            // Arrange
+            var start = "8,0";
+            var end = "0,8";
+
+            // Act
+            var line = new Line(start, end, true);
+
+            // Assert
+            line.Coordinates.Should().BeEmpty();
+        }
+    }
+}
---
+++ AdventOfCodeYear2021/Day5.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tidy.AdventOfCode;
+
+namespace AdventOfCodeYear2021
+{
+    [Day(2021, 5)]
+    public class Day5 : Day.NewLineSplitParsed<string>
+    {
+        public override object ExecutePart1()
+        {
+            var field = new Field(Input, true);
+            return field.GetAmountOfDuplicates();
+        }
+
+        public override object ExecutePart2()
+        {
+            var field = new Field(Input, false);
+            return field.GetAmountOfDuplicates();
+        }
+    }
+
+    public class Field
+    {
+        public List<Coordinate> HydrothermalVentsLocations { get; private set; }
+
+        public Field(string[] lineDescriptions, bool ignoreDiagonal)
+        {
+            var lines = lineDescriptions
+                .Select(descr => descr.Split(" -> ", StringSplitOptions.TrimEntries))
+                .Select(coordinatesDescription => new Line(coordinatesDescription[0], coordinatesDescription[1], ignoreDiagonal));
+            this.HydrothermalVentsLocations = lines.SelectMany(x => x.Coordinates).ToList();
+        }
+
+        public int GetAmountOfDuplicates()
+        {
+            return HydrothermalVentsLocations
+                .GroupBy(x => $"{x.X},{x.Y}")
+                .Count(x => x.Count() > 1);
+        }
+    }
+
+    public class Line
+    {
+        public List<Coordinate> Coordinates { get; private set; }
+
+        private List<IGetCoordinatesOfLineStrategy> _coordinatesInBetweenStrategies = new()
+            {
+                new HorizontalStrategy(),
+                new VerticalStrategy(),
+                new HorizontalRightToLeftStrategy(),
+                new VerticalBottomUpStrategy(),
+                new DiagonalLeftUpperToRightLower(),
+                new DiagonalRightUpperToLeftLower(),
+                new DiagonalRightLowerToLeftUpper(),
+                new DiagonalLeftLowerToRightUpper()
+            };
+
+        public Line(string startCoordinates, string endCoordinates, bool ignoreDiagonal)
+        {
+            var start = new Coordinate(startCoordinates);
+            var end = new Coordinate(endCoordinates);
+            this.Coordinates = AddCoordinates(start, end, ignoreDiagonal);
+        }
+
+        private List<Coordinate> AddCoordinates(Coordinate start, Coordinate end, bool ignoreDiagonal)
+        {
+            if (ignoreDiagonal && start.X != end.X && start.Y != end.Y)
+            {
+                return new List<Coordinate>();
+            }
+
+            var coordinates = new List<Coordinate> {start};
+
+            coordinates.AddRange(_coordinatesInBetweenStrategies
+                .First(_ => _.Applies(start, end))
+                .GetInBetween(start, end));
+
+            coordinates.Add(end);
+            return coordinates;
+        }
+    }
+
+    public class Coordinate
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public string Description => $"{X},{Y}";
+
+        public Coordinate(string coordinates)
+        {
+            var xAndYStart = coordinates.Split(",");
+            this.X = int.Parse(xAndYStart[0]);
+            this.Y = int.Parse(xAndYStart[1]);
+        }
+
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public interface IGetCoordinatesOfLineStrategy
+    {
+        public bool Applies(Coordinate start, Coordinate end);
+        public List<Coordinate> GetInBetween(Coordinate start, Coordinate end);
+    }
+
+    public class DiagonalLeftUpperToRightLower : IGetCoordinatesOfLineStrategy
+    {
+        public bool Applies(Coordinate start, Coordinate end)
+        {
+            return start.X < end.X && start.Y < end.Y;
+        }
+
+        public List<Coordinate> GetInBetween(Coordinate start, Coordinate end)
+        {
+            var coordinates = new List<Coordinate>();
+            var amountInBetween = end.X - start.X - 1;
+
+            for (int i = 1; i <= amountInBetween; i++)
+            {
+                coordinates.Add(new Coordinate(start.X + i, start.Y + i));
+            }
+
+            return coordinates;
+        }
+    }
+
+    public class DiagonalRightUpperToLeftLower : IGetCoordinatesOfLineStrategy
+    {
+        public bool Applies(Coordinate start, Coordinate end)
+        {
+            return start.X > end.X && start.Y < end.Y;
+        }
+
+        public List<Coordinate> GetInBetween(Coordinate start, Coordinate end)
+        {
+            var coordinates = new List<Coordinate>();
+            var amountInBetween = Math.Abs(end.X - start.X) - 1;
+
+            for (int i = 1; i <= amountInBetween; i++)
+            {
+                coordinates.Add(new Coordinate(start.X - i, start.Y + i));
+            }
+
+            return coordinates;
+        }
+    }
+
+    public class DiagonalRightLowerToLeftUpper : IGetCoordinatesOfLineStrategy
+    {
+        public bool Applies(Coordinate start, Coordinate end)
+        {
+            return start.X > end.X && start.Y > end.Y;
+        }
+
+        public List<Coordinate> GetInBetween(Coordinate start, Coordinate end)
+        {
+            return new DiagonalLeftUpperToRightLower().GetInBetween(end, start);
+        }
+    }
+
+    public class DiagonalLeftLowerToRightUpper : IGetCoordinatesOfLineStrategy
+    {
+        public bool Applies(Coordinate start, Coordinate end)
+        {
+            return start.X < end.X && start.Y > end.Y;
+        }
+
+        public List<Coordinate> GetInBetween(Coordinate start, Coordinate end)
+        {
+            return new DiagonalRightUpperToLeftLower().GetInBetween(end, start);
+        }
+    }
+
+    public class HorizontalStrategy : IGetCoordinatesOfLineStrategy
+    {
+        public bool Applies(Coordinate start, Coordinate end)
+        {
+            return start.Y == end.Y && start.X < end.X;
+        }
+
+        public List<Coordinate> GetInBetween(Coordinate start, Coordinate end)
+        {
+            var coordinates = new List<Coordinate>();
+            var amountInBetween = end.X - start.X - 1;
+
+            for (int i = 1; i <= amountInBetween; i++)
+            {
+                coordinates.Add(new Coordinate(start.X + i, start.Y));
+            }
+
+            return coordinates;
+        }
+    }
+
+    public class VerticalStrategy : IGetCoordinatesOfLineStrategy
+    {
+        public bool Applies(Coordinate start, Coordinate end)
+        {
+            return start.X == end.X && start.Y < end.Y;
+        }
+
+        public List<Coordinate> GetInBetween(Coordinate start, Coordinate end)
+        {
+            var coordinates = new List<Coordinate>();
+            var amountInBetween = end.Y - start.Y - 1;
+
+            for (int i = 1; i <= amountInBetween; i++)
+            {
+                coordinates.Add(new Coordinate(start.X, start.Y + i));
+            }
+
+            return coordinates;
+        }
+    }
+
+    public class HorizontalRightToLeftStrategy : IGetCoordinatesOfLineStrategy
+    {
+        public bool Applies(Coordinate start, Coordinate end)
+        {
+            return start.Y == end.Y && start.X > end.X;
+        }
+
+        public List<Coordinate> GetInBetween(Coordinate start, Coordinate end)
+        {
+            return new HorizontalStrategy().GetInBetween(end, start);
+        }
+    }
+
+    public class VerticalBottomUpStrategy : IGetCoordinatesOfLineStrategy
+    {
+        public bool Applies(Coordinate start, Coordinate end)
+        {
+            return start.X == end.X && start.Y > end.Y;
+        }
+
+        public List<Coordinate> GetInBetween(Coordinate start, Coordinate end)
+        {
+            return new VerticalStrategy().GetInBetween(end, start);
+        }
+    }
+}
