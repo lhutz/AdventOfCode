5d4527c114c996494c7222ae4fbdfec05e7c635f
Lindy Hutz <lindy.hutz@teamrockstars.nl>
2021-12-06 08:48:18 +0100

    Day 6.1

---
+++ AdventOfCodeYear2021.Test/Day6Test.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCodeYear2021.Test
+{
+    [TestFixture]
+    public class Day6Test
+    {
+        private int[] givenExample =
+        {
+            3,4,3,1,2
+        };
+
+        [Test]
+        public void ExecutePart1_GivenExample_ShouldReturnExpected()
+        {
+            // Arrange
+            var expectedValue = 5934;
+            var testInstance = new Day6 {Input = givenExample};
+
+            // Act
+            var receivedValue = testInstance.ExecutePart1();
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+
+        [Test]
+        public void ExecutePart2_GivenExample_ShouldReturnExpectedValue()
+        {
+            // Arrange
+            var expectedValue = 26984457539;
+            var testInstance = new Day6 {Input = givenExample};
+
+            // Act
+            var receivedValue = testInstance.ExecutePart2();
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+    }
+}
---
+++ AdventOfCodeYear2021/Day6.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tidy.AdventOfCode;
+
+namespace AdventOfCodeYear2021
+{
+    [Day(2021, 6)]
+    public class Day6 : Day<int[]>
+    {
+        public override int[] ParseInput(string rawInput) => rawInput
+            .Split(",", StringSplitOptions.TrimEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        public override object ExecutePart1()
+        {
+            return GetAmountOfFishAfterDays(80);
+        }
+
+        public override object ExecutePart2()
+        {
+            return GetAmountOfFishAfterDays(256);
+        }
+
+        private object GetAmountOfFishAfterDays(int days)
+        {
+            return Input
+                .Select(_ => new LanternFish(_).DaysPasses(days))
+                .Sum(fish => fish.GetAmountOfOffspringPlusMe());
+        }
+    }
+
+    public class LanternFish
+    {
+        public int InternalTimer { get; private set; }
+        private readonly List<LanternFish> _children;
+        public LanternFish()
+        {
+            InternalTimer = 8;
+            _children = new List<LanternFish>();
+        }
+
+        public LanternFish(int internalTimer)
+        {
+            InternalTimer = internalTimer;
+            _children = new List<LanternFish>();
+        }
+
+        public LanternFish DaysPasses(int days)
+        {
+            for (int i = 1; i <= days; i++)
+            {
+                InternalTimer--;
+                if (InternalTimer < 0)
+                {
+                    _children.Add(new LanternFish().DaysPasses(days - i));
+                    InternalTimer = 6;
+                }
+            }
+
+            return this;
+        }
+
+        public long GetAmountOfOffspringPlusMe()
+        {
+            return 1 + _children.Sum(lanternFish => lanternFish.GetAmountOfOffspringPlusMe());
+        }
+
+    }
+}
