8af5a994f714cb2224de132013e79ef33601cfe4
Lindy Hutz <lindy.hutz@teamrockstars.nl>
2021-12-08 10:41:09 +0100

    Day8

---
+++ AdventOfCodeYear2021.Test/Day8Test.cs
@@ -0,0 +1,227 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCodeYear2021.Test
+{
+    [TestFixture]
+    public class Day8Test
+    {
+        private string[][][] givenExample =
+        {
+            new [] {new [] {"be", "cfbegad", "cbdgef", "fgaecd", "cgeb","fdcge","agebfd","fecdb","fabcd","edb"}, new [] {"fdgacbe", "cefdb","cefbgd","gcbe"}},
+            new [] {new [] {"edbfga", "begcd", "cbg", "gc", "gcadebf","fbgde","acbgfd","abcde","gfcbed","gfec"}, new [] {"fcgedb", "cgb","dgebacf","gc"}},
+            new [] {new [] {"fgaebd", "cg", "bdaec", "gdafb", "agbcfd","gdcbef","bgcad","gfac","gcb","cdgabef"}, new [] {"cg", "cg","fdcagb","cbg"}},
+            new [] {new [] {"fbegcd", "cbd", "adcefb", "dageb", "afcb","bc","aefdc","ecdab","fgdeca","fcdbega"}, new [] {"efabcd", "cedba","gadfec","cb"}},
+            new [] {new [] {"aecbfdg", "fbg", "gf", "bafeg", "dbefa","fcge","gcbea","fcaegb","dgceab","fcbdga"}, new [] {"gecf", "egdcabf","bgf","bfgea"}},
+            new [] {new [] {"fgeab", "ca", "afcebg", "bdacfeg", "cfaedg","gcfdb","baec","bfadeg","bafgc","acf"}, new [] {"gebdcfa", "ecba","ca","fadegcb"}},
+            new [] {new [] {"dbcfg", "fgd", "bdegcaf", "fgec", "aegbdf","ecdfab","fbedc","dacgb","gdcebf","gf"}, new [] {"cefg", "dcbef","fcge","gbcadfe"}},
+            new [] {new [] {"bdfegc", "cbegaf", "gecbf", "dfcage", "bdacg","ed","bedf","ced","adcbefg","gebcd"}, new [] {"ed", "bcgafe","cdgba","cbgef"}},
+            new [] {new [] {"egadfb", "cdbfeg", "cegd", "fecab", "cgb","gbdefca","cg","fgcdab","egfdb","bfceg"}, new [] {"gbdfcae", "bgc","cg","cgb"}},
+            new [] {new [] {"gcafb", "gcf", "dcaebfg", "ecagb", "gf","abcdeg","gaef","cafbge","fdbac","fegbdc"}, new [] {"fgae", "cfgab","fg","bagce"}},
+        };
+
+        [Test]
+        public void ExecutePart1_GivenExample_ShouldReturnExpected()
+        {
+            // Arrange
+            var expectedValue = 26;
+            var testInstance = new Day8 {Input = givenExample};
+
+            // Act
+            var receivedValue = testInstance.ExecutePart1();
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+
+        [Test]
+        public void ExecutePart2_GivenExample_ShouldReturnExpectedValue()
+        {
+            // Arrange
+            var expectedValue = 61229;
+            var testInstance = new Day8 {Input = givenExample};
+
+            // Act
+            var receivedValue = testInstance.ExecutePart2();
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+
+        [Test]
+        public void GetOutputValue_InputExample_5353()
+        {
+            // Arrange
+            var line = new []
+            {
+                new [] {"acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb","ab"},
+                new [] {"cdfeb", "fcadb", "cdfeb", "cdbaf"}
+            };
+            var expectedValue = 5353;
+
+            // Act
+            var receivedValue = new Day8().GetOutputValue(line);
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+
+        [Test]
+        public void GetCode_InputExample_CorrectCode()
+        {
+            // Arrange
+            var line = new []
+            {
+                new [] {"acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb","ab"},
+                new [] {"cdfeb fcadb cdfeb cdbaf"}
+            };
+            var expectedValue = new [] { "cagedb", "ab", "gcdfa", "fbcad", "eafb", "cdfbe", "cdfgeb", "dab", "acedgfb", "cefabd" };
+
+            // Act
+            var receivedValue = new Day8().GetCode(line[0]);
+
+            // Assert
+            receivedValue.Should().ContainInOrder(expectedValue);
+        }
+
+        [Test]
+        public void GetOutputValue_Input0_8394()
+        {
+            // Arrange
+            var line = givenExample[0];
+            var expectedValue = 8394;
+
+            // Act
+            var receivedValue = new Day8().GetOutputValue(line);
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+
+        [Test]
+        public void GetOutputValue_Input1_9781()
+        {
+            // Arrange
+            var line = givenExample[1];
+            var expectedValue = 9781;
+
+            // Act
+            var receivedValue = new Day8().GetOutputValue(line);
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+
+        [Test]
+        public void GetOutputValue_Input2_1197()
+        {
+            // Arrange
+            var line = givenExample[2];
+            var expectedValue = 1197;
+
+            // Act
+            var receivedValue = new Day8().GetOutputValue(line);
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+
+        [Test]
+        public void GetOutputValue_Input3_9361()
+        {
+            // Arrange
+            var line = givenExample[3];
+            var expectedValue = 9361;
+
+            // Act
+            var receivedValue = new Day8().GetOutputValue(line);
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+
+        [Test]
+        public void GetOutputValue_Input4_4873()
+        {
+            // Arrange
+            var line = givenExample[4];
+            var expectedValue = 4873;
+
+            // Act
+            var receivedValue = new Day8().GetOutputValue(line);
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+
+        [Test]
+        public void GetOutputValue_Input5_8418()
+        {
+            // Arrange
+            var line = givenExample[5];
+            var expectedValue = 8418;
+
+            // Act
+            var receivedValue = new Day8().GetOutputValue(line);
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+
+        [Test]
+        public void GetOutputValue_Input6_4548()
+        {
+            // Arrange
+            var line = givenExample[6];
+            var expectedValue = 4548;
+
+            // Act
+            var receivedValue = new Day8().GetOutputValue(line);
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+
+        [Test]
+        public void GetOutputValue_Input7_1625()
+        {
+            // Arrange
+            var line = givenExample[7];
+            var expectedValue = 1625;
+
+            // Act
+            var receivedValue = new Day8().GetOutputValue(line);
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+
+        [Test]
+        public void GetOutputValue_Input8_8717()
+        {
+            // Arrange
+            var line = givenExample[8];
+            var expectedValue = 8717;
+
+            // Act
+            var receivedValue = new Day8().GetOutputValue(line);
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+
+        [Test]
+        public void GetOutputValue_Input9_4315()
+        {
+            // Arrange
+            var line = givenExample[9];
+            var expectedValue = 4315;
+
+            // Act
+            var receivedValue = new Day8().GetOutputValue(line);
+
+            // Assert
+            receivedValue.Should().Be(expectedValue);
+        }
+    }
+}
---
+++ AdventOfCodeYear2021/Day8.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tidy.AdventOfCode;
+
+namespace AdventOfCodeYear2021
+{
+    [Day(2021, 8)]
+    public class Day8 : Day<string[][][]>
+    {
+        public override string[][][] ParseInput(string rawInput)
+        {
+            return rawInput.Split('\n')
+                .Select(_ =>
+                {
+                    var inputAndOutput = _.Split(" | ", StringSplitOptions.TrimEntries);
+                    var input = inputAndOutput[0].Split(" ", StringSplitOptions.TrimEntries);
+                    var output = inputAndOutput[1].Split(" ", StringSplitOptions.TrimEntries);
+                    return new[] {input, output};
+                }).ToArray();
+        }
+
+        public override object ExecutePart1()
+        {
+            var lengthOfUniqueSegments = new[] {2, 4, 3, 7};
+            return Input
+                .Sum(inputLine => inputLine[1]
+                    .Count(output => lengthOfUniqueSegments.Contains(output.Length)));
+        }
+
+        public override object ExecutePart2()
+        {
+            return Input.Sum(GetOutputValue);
+        }
+
+        public int GetOutputValue(string[][] line)
+        {
+            var code = this.GetCode(line[0]).ToList();
+            var decrypted = "";
+            for (var i = 0; i < line[1].Length; i++)
+                decrypted += Decrypt(code, line[1][i]);
+
+            return int.Parse(decrypted);
+        }
+
+        private string Decrypt(List<string> code, string value)
+        {
+            return code.IndexOf(code.First(_ => new HashSet<char>(_).SetEquals(value))).ToString();
+        }
+
+        public string[] GetCode(string[] input)
+        {
+            var oneValue = input.First(_ => _.Length == 2);
+            var fourValue = input.First(_ => _.Length == 4);
+            var sevenValue = input.First(_ => _.Length == 3);
+            var eighthValue = input.First(_ => _.Length == 7);
+
+            var twoThreeFiveValue = input.Where(_ => _.Length == 5).ToArray();
+            var sixNineZeroValue = input.Where(_ => _.Length == 6).ToArray();
+
+            var threeValue = twoThreeFiveValue.First(three => oneValue.All(three.Contains));
+            var nineValue = sixNineZeroValue.First(nine => fourValue.All(nine.Contains));
+            var zeroValue = sixNineZeroValue.First(zero => oneValue.All(zero.Contains) && zero != nineValue);
+            var sixValue = sixNineZeroValue.First(six => !new [] {nineValue, zeroValue}.Contains(six));
+            var fiveValue = twoThreeFiveValue.First(five => five.All(sixValue.Contains));
+            var twoValue = twoThreeFiveValue.First(two => !new [] {threeValue, fiveValue}.Contains(two));
+            return new[]
+            {
+                zeroValue, oneValue, twoValue, threeValue, fourValue,
+                fiveValue, sixValue, sevenValue, eighthValue, nineValue
+            };
+        }
+    }
+}
